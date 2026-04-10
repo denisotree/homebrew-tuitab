69070eb13787fb42ae38a4ac65471483e5c3ed1d
denisotree <denisotree@gmail.com>
2026-04-10 10:50:19 +0100

    tuitab 0.1.2

--- Formula/tuitab.rb
+++ Formula/tuitab.rb
@@ -19,26 +19,26 @@ class Tuitab < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/denisotree/tuitab/releases/download/v0.1.1/tuitab-v0.1.1-aarch64-apple-darwin.tar.gz"
-      sha256 "32fa88ea3e349e41ad838d6fdb8b617098348da1e0fd9fbf5eedc18c2c4658c0"
-      version "0.1.1"
+      url "https://github.com/denisotree/tuitab/releases/download/v0.1.2/tuitab-v0.1.2-aarch64-apple-darwin.tar.gz"
+      sha256 "02a89b3541096898d7983bbc392ff2f6eb02a37d51177697339bcb1669df46c6"
+      version "0.1.2"
     end
     on_intel do
-      url "https://github.com/denisotree/tuitab/releases/download/v0.1.1/tuitab-v0.1.1-x86_64-apple-darwin.tar.gz"
-      sha256 "afdafcd695ccc459098d4b1c40a40ac7e23de5f03a7ba5d737159581b1e6bc3f"
-      version "0.1.1"
+      url "https://github.com/denisotree/tuitab/releases/download/v0.1.2/tuitab-v0.1.2-x86_64-apple-darwin.tar.gz"
+      sha256 "f8c100ece80ac677a84a2a059c1932a75da48711ca46bb46248d22ee832a30d0"
+      version "0.1.2"
     end
   end
   on_linux do
     on_arm do
-      url "https://github.com/denisotree/tuitab/releases/download/v0.1.1/tuitab-v0.1.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "2a1cf4dcd7415f8532e87c4a8707b8d948f4f274b84699da69ef44afb6027584"
-      version "0.1.1"
+      url "https://github.com/denisotree/tuitab/releases/download/v0.1.2/tuitab-v0.1.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "05a01327e31684db70f45aa85f669d1b46923503c68130f9ebff869184bf4b2d"
+      version "0.1.2"
     end
     on_intel do
-      url "https://github.com/denisotree/tuitab/releases/download/v0.1.1/tuitab-v0.1.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "c54975320295a98db1a2420b255bb961eacc8cd9cb7c6f44ceb92d30196484b1"
-      version "0.1.1"
+      url "https://github.com/denisotree/tuitab/releases/download/v0.1.2/tuitab-v0.1.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "789016f25dc64ce0451d1ad99c8896f617f3e2784834637c63060cfac0707854"
+      version "0.1.2"
     end
   end
 
