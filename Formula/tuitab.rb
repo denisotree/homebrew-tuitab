71a69a1ab8590502da4dd4bb24ffad0b7d60e6b6
Denis Vdovin <denisotree@gmail.com>
2026-04-15 12:17:57 +0100

    chore: update formula to v0.1.5

--- Formula/tuitab.rb
+++ Formula/tuitab.rb
@@ -19,26 +19,26 @@ class Tuitab < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/denisotree/tuitab/releases/download/v0.1.2/tuitab-v0.1.2-aarch64-apple-darwin.tar.gz"
-      sha256 "02a89b3541096898d7983bbc392ff2f6eb02a37d51177697339bcb1669df46c6"
-      version "0.1.2"
+      url "https://github.com/denisotree/tuitab/releases/download/v0.1.5/tuitab-v0.1.5-aarch64-apple-darwin.tar.gz"
+      sha256 "3ac6b029d56d41880163044d0b4e8fef555bbfd901d20516e671d0f80723c9cf"
+      version "0.1.5"
     end
     on_intel do
-      url "https://github.com/denisotree/tuitab/releases/download/v0.1.2/tuitab-v0.1.2-x86_64-apple-darwin.tar.gz"
-      sha256 "f8c100ece80ac677a84a2a059c1932a75da48711ca46bb46248d22ee832a30d0"
-      version "0.1.2"
+      url "https://github.com/denisotree/tuitab/releases/download/v0.1.5/tuitab-v0.1.5-x86_64-apple-darwin.tar.gz"
+      sha256 "80d434257ea9dec8b4f8330ea95b69a3fe4c31b39d87093c5a95b972a951acdc"
+      version "0.1.5"
     end
   end
   on_linux do
     on_arm do
-      url "https://github.com/denisotree/tuitab/releases/download/v0.1.2/tuitab-v0.1.2-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "05a01327e31684db70f45aa85f669d1b46923503c68130f9ebff869184bf4b2d"
-      version "0.1.2"
+      url "https://github.com/denisotree/tuitab/releases/download/v0.1.5/tuitab-v0.1.5-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "5f94bf42d7001abaebb217f6b2c5f75ccfcb2fdbc09948071980608d28fd798b"
+      version "0.1.5"
     end
     on_intel do
-      url "https://github.com/denisotree/tuitab/releases/download/v0.1.2/tuitab-v0.1.2-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "789016f25dc64ce0451d1ad99c8896f617f3e2784834637c63060cfac0707854"
-      version "0.1.2"
+      url "https://github.com/denisotree/tuitab/releases/download/v0.1.5/tuitab-v0.1.5-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "98ada82325a3a598118b7ea023432c7d1f8bd8dd43ec60d822092fb3928ce7e7"
+      version "0.1.5"
     end
   end
 
